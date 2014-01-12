05feeda4e9af6d8591cc7107a3c0f96821147c83
Mark Lamley <marksl.git@gmail.com>
2014-01-12 09:56:01 -0800

    missing file

---
+++ ALTest.Core/TestResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ALTest.Core
+{
+    [Serializable]
+    public class TestResult : MarshalByRefObject
+    {
+        public TestResult(string testName, bool testPassed,
+                          string className, string exceptionString)
+            : this()
+        {
+            TestName = testName;
+            TestPassed = testPassed;
+            ClassName = className;
+            ExceptionString = exceptionString;
+        }
+
+        public TestResult()
+        {
+        }
+
+        public string ClassName { get; set; }
+        public string TestName { get; set; }
+        public bool TestPassed { get; set; }
+        public string ExceptionString { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}", TestName, TestPassed);
+        }
+    }
+}
